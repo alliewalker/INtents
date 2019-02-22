d1fd8aa0d9530f4626035c221ee5f8b90f41fc40
alliewalker <walkera71193@gmail.com>
2019-02-22 14:31:00 -0500

    review table

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 let BASE_URL = 'http://localhost:3000'
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    authorization: sessionStorage.getItem("token")
+  })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -11,21 +17,42 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   login(email, password) {
-    return this.http.post<HasToken & HasUser>(`${BASE_URL}/user/login`, {
+    let request =  this.http.post<HasToken & HasUser>(`${BASE_URL}/user/login`, {
       user:{
         email: email,
         password: password
       }
+    }, httpOptions)
+    request.subscribe(({ token }) => {
+      httpOptions.headers=new HttpHeaders().set('Content-Type', 'application/json').set('authorization', token);
     })
+    return request;
   }
 
   signup(email, password) {
-    return this.http.post<HasToken & HasUser>(`${BASE_URL}/user/create`, {
+    let request = this.http.post<HasToken & HasUser>(`${BASE_URL}/user/create`, {
       user: {
         email: email,
         password: password
       }
-    })
+    }, httpOptions)
+    request.subscribe(({ token }) => {
+      httpOptions.headers=new HttpHeaders().set('Content-Type', 'application/json').set('authorization', token);
+  })
+  return request;
+}
+
+  createReview(rating: number, message: string) {
+    return this.http.post<HasCreated<Review>>(`${BASE_URL}/review/make`, {
+      review: {
+        starRating: rating,
+        review: message
+      }
+    }, httpOptions)
+  }
+
+  getReviews() {
+    return this.http.get<Review[]>(`${BASE_URL}/review/read`, httpOptions)
   }
 }
 
@@ -35,5 +62,21 @@ interface HasToken {
 
 interface HasUser {
   user: object
+}
+
+interface HasMessage {
+  message: string
+}
+
+interface HasCreated<T> {
+  created: T
+}
+
+class Review {
+  id: number;
+  starRating: number;
+  review: string;
+  updatedAt: string;
+  createdAt: string;
 }
 
